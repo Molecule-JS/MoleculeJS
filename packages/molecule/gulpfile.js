58807acccc5afede360a81cf55822fd51765841d
DiiLord <d.drodt@gmx.de>
2017-10-11 19:59:17 +0200

    Removed gulpfile from gitignore

---
+++ packages/molecule/gulpfile.js
@@ -0,0 +1,13 @@
+const gulp = require('gulp');
+const minify = require('gulp-minify');
+
+gulp.task('default',() => {
+    gulp.src('lit-element.js')
+        .pipe(minify({
+            ext: {
+                min: '.min.js'
+            },
+            noSource: true
+        }))
+        .pipe(gulp.dest('.'))
+})
