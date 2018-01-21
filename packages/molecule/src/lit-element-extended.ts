2d17e4b35e3d6e99a9a833e70044e954e25df0f0
DiiLord <d.drodt@gmx.de>
2018-01-21 22:47:38 +0100

    Fix ES imports for npm

--- packages/molecule/src/lit-element-extended.ts
+++ packages/molecule/src/lit-element-extended.ts
@@ -1,5 +1,5 @@
-import { svg, TemplateResult } from '../node_modules/lit-html/lit-html.js';
-import { html, render } from '../node_modules/lit-html/lib/lit-extended.js';
+import { svg, TemplateResult } from '../lit-html/lit-html.js';
+import { html, render } from '../lit-html/lib/lit-extended.js';
 import { LitLite, HTMLClass, HTMLCollectionByID, data, propConfig, properties, camelCaseToKebab, methodsToCall } from './lit-lite.js';
 
 export const LitElementExtended = (superclass: HTMLClass) => LitLite(superclass, html, render);
--- packages/molecule/src/lit-element.ts
+++ packages/molecule/src/lit-element.ts
@@ -1,4 +1,4 @@
-import { html, svg, render, TemplateResult } from '../node_modules/lit-html/lit-html.js';
+import { html, svg, render, TemplateResult } from '../lit-html/lit-html.js';
 import { LitLite, HTMLClass, HTMLCollectionByID, data, propConfig, properties, camelCaseToKebab, methodsToCall, LitEventInit } from './lit-lite.js';
 
 export const LitElement = (superclass: HTMLClass) => LitLite(superclass, html, render);
