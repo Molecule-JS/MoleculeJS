4b79e01833c6ed74ec8af446fb7d6f5665caf560
DiiLord <d.drodt@gmx.de>
2018-05-07 19:01:22 +0200

    Delete autogenerated files

--- packages/molecule/molecule-decorators.d.ts
+++
@@ -1,3 +0,0 @@
-import { PropConfig } from './molecule.js';
-export declare function property(config?: PropConfig): (prototype: any, propName: string) => void;
-export declare function attribute(config?: PropConfig): (prototype: any, propName: string) => void;
--- packages/molecule/molecule-lit-extended.js
+++
@@ -1,7 +0,0 @@
-import { svg, TemplateResult } from '../lit-html/lit-html.js';
-import { html, render } from '../lit-html/lib/lit-extended.js';
-import { Molecule, camelCaseToKebab } from './molecule.js';
-export const MoleculeLitExtended = (superclass = HTMLElement) => Molecule(superclass, render);
-export { html, svg, TemplateResult };
-export { camelCaseToKebab };
-//# sourceMappingURL=molecule-lit-extended.js.map
