30462fd575b55bcf323c402085a5ca304c367f17
DiiLord <d.drodt@gmx.de>
2018-06-12 10:22:24 +0200

    Remov eold stuff

--- packages/molecule/src/molecule-lit.ts
+++
@@ -1,9 +0,0 @@
-import { html, svg, render, TemplateResult, SVGTemplateResult } from '../node_modules/lit-html/lit-html';
-import { Molecule, HTMLCollectionByID, PropConfig, Properties, camelCaseToKebab } from './molecule';
-
-export const MoleculeLit = Molecule(render);
-
-export { html, svg, TemplateResult, SVGTemplateResult };
-export { HTMLCollectionByID, PropConfig, Properties, camelCaseToKebab };
-
-export default { MoleculeLit, Molecule, html, svg, render };
