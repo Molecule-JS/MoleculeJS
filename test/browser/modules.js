5d4c8822ee1844abee5b7cda565deb1ba0e4d626
DerDrodt <d.drodt@gmx.de>
2018-12-16 16:31:41 +0100

    chore: Rremove old test setup

--- test/browser/modules.js
+++
@@ -1,9 +0,0 @@
-import '/tests/molecule-decorators.test.mjs';
-import '/tests/molecule.test.mjs';
-import '/tests/molecule-lit.test.mjs';
-import '/tests/molecule-lit-extended.test.mjs';
-import '/tests/molecule-functional.test.mjs';
-import '/tests/molecule-functional-lit.test.mjs';
-import '/tests/molecule-functional-lit-extended.test.mjs';
-import '/tests/molecule-lit-directive-set-element.test.mjs';
-import '/tests/molecule-jsx.test.mjs';
